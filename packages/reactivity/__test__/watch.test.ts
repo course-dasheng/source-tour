d188b68759e789e0af77626edd375e933aceb6e6
花果山大圣 <316783812@qq.com>
2022-05-14 18:31:05 +0800

    08-watcj

--- packages/reactivity/__test__/watch.test.ts
+++ packages/reactivity/__test__/watch.test.ts
@@ -10,7 +10,9 @@ describe('测试watch监听器',()=>{
   it('watch基本使用',()=>{
     let data = {age:1}
     let obj = reactive(data)
-    let fn = vi.fn((a1,a2)=>{})
+    let fn = vi.fn((a1,a2)=>{
+      // console.log(a1,a2)
+    })
     watch(()=>obj.age,(newValue,oldValue)=>{
       fn(newValue,oldValue)
     })
@@ -20,5 +22,26 @@ describe('测试watch监听器',()=>{
     expect(fn).toHaveBeenCalledWith(2,1)
   })
 
+  it('watch对象',async ()=>{
+    let data = {age:1,name:'大圣'}
+    let obj = reactive(data)
+    let fn = vi.fn((a1,a2)=>{
+    })
+    watch(obj,(oldValue,newValue)=>{
+      fn(oldValue,newValue)
+    },{
+      immediate:true,
+      flush:'post'
+    })
+    expect(fn).toHaveBeenCalledTimes(1)
+    expect(fn).toHaveBeenCalledWith(
+      {age:1,name:'大圣'},
+      undefined
+    )
+    obj.age++
+    expect(fn).toHaveBeenCalledTimes(1)
+    await vi.runAllTicks()
+    expect(fn).toHaveBeenCalledTimes(2)
+  })
 })
 
--- packages/reactivity/src/watch.ts
+++ packages/reactivity/src/watch.ts
@@ -2,28 +2,49 @@ import {effect} from './effect'
 
 // watch本质也是一个effect，配合scheduler监听变化
 
-
+function traverse(value, seen = new Set()) {
+  if (typeof value !== 'object' || value === null || seen.has(value)) return
+  seen.add(value)
+  for (const k in value) {
+    traverse(value[k], seen)
+  }
+  return value
+}
 
 export function watch(source, cb, options = {}) {
   let getter
   if (typeof source === 'function') {
     getter = source
-  } 
+  } else {
+    // source是对象，就需要多次修改合并更新
+    getter = () => traverse(source)
+  }
   let newValue,oldValue
+  const job = () => {
+    newValue = effectFn()
+    cb(newValue,oldValue)
+    oldValue = newValue
+  }
   const effectFn = effect(
     // 执行 getter
-    getter,
+    () => getter(),
     {
       lazy: true,
       scheduler: () => {
-        newValue = effectFn()
-        console.log(234,newValue,oldValue)
-        cb(newValue, oldValue)
-        oldValue = newValue
+        if (options.flush === 'post') {
+          const p = Promise.resolve()
+          p.then(job)
+        } else {
+          job()
+        }
       }
     }
   )
-  oldValue = effectFn()
+  if (options.immediate) {
+    job()
+  } else {
+    oldValue = effectFn()
+  }
   // console.log(123,oldValue)
 
 }
