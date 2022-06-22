0bbdb0aab4adcd72c3b5473b8df1702608d0def2
花果山大圣 <316783812@qq.com>
2022-06-22 11:45:33 +0800

    docs: add docs

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,76 @@
+
+export default {
+  title: '前端源码漫游记',
+  description: '前端框架源码一网打尽|Vue3|React|Vite|Cli',
+  head: [
+    ['link', { rel: 'icon', href: 'https://cdn.jsdelivr.net/gh/shengxinjing/static/element3.ico', type: 'image/svg+xml' }],
+    ['link', { rel: 'alternate icon', href: 'https://cdn.jsdelivr.net/gh/shengxinjing/static/hugsun.png', type: 'image/png', sizes: '16x16' }],
+  ],
+  // base:"/src/"
+  themeConfig: {
+    logo: 'https://cdn.jsdelivr.net/gh/shengxinjing/static/hugsun.png',
+    nav: [
+      { text: 'Vue3', link: '/vue/' },
+      { text: 'React', link: '/react/' },
+      { text: 'Vite', link: '/vite/' },
+      { text: '框架设计基础', link: '/guide/design' },
+    ],
+    socialLinks: [
+      { icon: 'discord', link: 'https://discord.com/channels/987387170232868904/987387170794922005' },
+      { icon: 'github', link: 'https://github.com/course-dasheng/fe-source-tour/' },
+    ],
+    sidebar: {
+      '/': [
+        {
+          text: '教程',
+          items: [
+            { text: '开发步骤', link: '/guide/step' },
+            { text: '开发规范', link: '/guide/scripts' },
+            { text: '框架设计基础知识', link: '/guide/design' },
+          ],
+        },
+        {
+          text: 'Vue',
+          items: [
+            { text: 'Vue3架构', link: '/vue/' },
+            { text: '响应式', link: '/vue/reactive/' },
+            { text: '路由vue-router', link: '/vue/reactive/' },
+          ],
+        },
+        {
+          text: 'React',
+          items: [
+            { text: 'React架构介绍', link: '/react/' },
+            { text: '组件', link: '/react/container/' },
+            { text: '路由', link: '/react/container/' },
+            { text: '数据管理', link: '/react/container/' },
+          ],
+        },
+        {
+          text: '工程化',
+          items: [
+            { text: 'webpack', link: '/components/badge/' },
+            { text: 'rollup', link: '/components/badge/' },
+            { text: 'vite（重要）', link: '/components/badge/' },
+            { text: '命令行工具cli', link: '/components/badge/' },
+          ],
+        },
+        {
+          text: '工具库',
+          items: [
+            { text: '工具函数', link: '/components/badge/' },
+            { text: 'chrome开发工具', link: '/components/badge/' },
+            { text: 'Vscode插件', link: '/components/badge/' },
+            { text: 'Vscode插件', link: '/components/badge/' },
+
+          ],
+        },
+
+      ],
+    },
+  },
+  markdown: {
+    config: (md) => {
+    },
+  },
+}
---
+++ docs/index.md
@@ -0,0 +1,38 @@
+---
+layout: home
+sidebar: false
+
+title: 前端源码漫游记 
+titleTemplate: 前端框架源码一网打尽|Vue3|React|Vite|Cli
+
+hero:
+  name: 源码漫游记
+  text: 前端框架源码一网打尽
+  tagline: 一次性学完热门前端框架原理
+  image:
+    src: https://cdn.jsdelivr.net/gh/shengxinjing/static/pair.png
+    alt: Element3
+  actions:
+    - theme: brand
+      text: Get Started
+      link: /guide
+    - theme: alt
+      text: Vue
+      link: /vue/
+    - theme: alt
+      text: React
+      link: /react/
+    - theme: alt
+      text: Vite
+      link: /vite/
+
+features:
+  - title: 热门框架源码
+    details: Vue3，React，Vite一网打尽
+  - title: TDD开发模式
+    details: 测试驱动开发，基于vitest
+  - title: 学习专用
+    details: 学习各大框架原理最好项目
+  - title: 最新技术栈
+    details: TypeScript  JSX pnpm  
+---
